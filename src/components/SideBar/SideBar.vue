4a484477aa0e5102cf2f4406ce00e360eed95240
Abstrekt <saammmmmmy@gmail.com>
2021-01-25 23:21:33 +0530

    update sidebar

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -9,29 +9,22 @@
     v-model="navbar"
   >
     <v-list>
-      <v-list-item class="px-2">
+      <v-list-item class="px-2" >
         <v-list-item-avatar>
           <v-img src="@/assets/ipl.jpg"></v-img>
         </v-list-item-avatar>
-        <!-- </v-list-item> -->
 
-        <!-- <v-list-item link> -->
         <v-list-item-content>
           <v-list-item-title class="title">
             Search IPL
           </v-list-item-title>
-          <!-- <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle> -->
         </v-list-item-content>
       </v-list-item>
     </v-list>
 
-    <!-- <v-divider></v-divider> -->
-
     <v-list shaped nav dense>
-      <!-- <v-subheader class="truncate">Category</v-subheader> -->
-
-      <v-list-item-group v-model="selectedItem" color="primary">
-        <v-list-item v-for="(item, i) in sidebarItems" :key="i">
+      <v-list-item-group mandatory v-model="selectedItem" color="primary">
+        <v-list-item v-for="(item, i) in sidebarItems" :key="i" :ripple="false">
           <v-list-item-icon>
             <v-icon v-text="item.icon"></v-icon>
           </v-list-item-icon>
@@ -62,52 +55,3 @@ export default {
   }),
 };
 </script>
-
-<!--
-<template>
-  <div class="ma-12 pa-12">
-    <v-card>
-      <v-navigation-drawer app permanent width="350">
-        <v-list>
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-            </v-list-item-avatar>
-          </v-list-item> 
-
-          <v-list-item link>
-            <v-list-item-content>
-              <v-list-item-title class="title">
-                Search IPL
-              </v-list-item-title>
-              <v-list-item-subtitle>Facet Filter for Searching IPL.</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list dense>
-          <v-list-group sub-group ripple>
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>Teams</v-list-item-title>
-              </v-list-item-content>
-            </template>
-
-            <v-list-item v-for="(team, i) in teams" :key="i">
-              <v-list-item-action>
-                <v-checkbox :input-value="active"></v-checkbox>
-              </v-list-item-action>
-
-              <v-list-item-content>
-                <v-list-item-title v-text="team"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
-        </v-list>
-      </v-navigation-drawer>
-    </v-card>
-  </div>
-</template>
--->
