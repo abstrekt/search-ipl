67086ca1fcac2b4e97fa131622ae42b3f4e4502c
Abstrekt <saammmmmmy@gmail.com>
2021-01-25 22:25:24 +0530

    add pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,8 @@
       </v-col>
       <v-col cols="9">
         <v-row align="center">
-          <v-col cols="8"
-            ><v-breadcrumbs
+          <v-col cols="8">
+            <v-breadcrumbs
               :items="[
                 {
                   text: 'Dashboard',
@@ -15,7 +15,7 @@
                   href: '/',
                 },
                 {
-                  text: 'Search',
+                  text: 'Players',
                   disabled: false,
                   href: '',
                 },
@@ -29,8 +29,8 @@
               <template v-slot:divider>
                 <v-icon>mdi-chevron-right</v-icon>
               </template>
-            </v-breadcrumbs></v-col
-          >
+            </v-breadcrumbs>
+          </v-col>
           <v-col cols="2">
             <v-select
               color=""
@@ -64,28 +64,32 @@
           </v-col>
         </v-row>
         <v-row>
-          <v-container grid-list-xs>
-            <v-row>
-              <v-col cols="4" v-for="n in 25" :key="n">
-                <v-card class="mx-auto" max-width="400">
-                  <v-img
-                    class="white--text align-end"
-                    height="200px"
-                    src="@/assets/players/virat.jpg"
-                  >
-                    <v-card-title>Viral Kohli</v-card-title>
-                  </v-img>
-                  <v-card-subtitle class="pb-0"> Mumbai Indians</v-card-subtitle>
-
-                  <v-card-text class="text--primary">
-                    <div>10-06-1981 • Right Hand • Right-arm medium • India</div>
-
-                    <!-- <div>Whitsunday Island, Whitsunday Islands</div> -->
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
+            <v-container grid-list-xs class="results-container">
+              <v-row>
+                <v-col cols="4" v-for="n in 25" :key="n">
+                  <v-card class="mx-auto" max-width="400">
+                    <v-img
+                      class="white--text align-end"
+                      height="200px"
+                      src="@/assets/players/virat.jpg"
+                    >
+                      <v-card-title>Viral Kohli</v-card-title>
+                    </v-img>
+                    <v-card-subtitle class="pb-0"> Mumbai Indians</v-card-subtitle>
+                    <v-card-text class="text--primary">
+                      <div>10-06-1981 • Right Hand • Right-arm medium • India</div>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          <v-row class="mt-5">
+            <v-col>
+              <div class="text-center">
+                <v-pagination v-model="page" :length="4" circle></v-pagination>
+              </div>
+            </v-col>
+          </v-row>
         </v-row>
       </v-col>
     </v-row>
@@ -95,14 +99,19 @@
 <script>
 // @ is an alias to /src
 import FacetFilter from '@/components/Filter/FacetFilter';
-
 export default {
   name: 'Home',
   data: () => ({
     sort: '',
+    page: 1,
   }),
   components: { FacetFilter },
 };
 </script>
 
-<style lang="sass"></style>
+<style lang="scss">
+.results-container {
+  height: 1000px;
+  overflow-y: scroll;
+}
+</style>
