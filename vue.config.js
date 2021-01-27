2c3d01103d3c2fd827445300b85591c076c2970d
Abstrekt <saammmmmmy@gmail.com>
2021-01-27 15:41:50 +0530

    enable compression + prefetch

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,39 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ['vuetify'],
+  chainWebpack(config) {
+    config.plugins.delete('prefetch');
+  },
+  pluginOptions: {
+    compression: {
+      brotli: {
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      },
+    },
+  },
+  // configureWebpack: {
+  //   plugins: [
+  //     new CompressionPlugin({
+  //       filename: '[path].gz[query]',
+  //       algorithm: 'gzip',
+  //       test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+  //       threshold: 10240,
+  //       minRatio: 0.2,
+  //     }),
+  //   ],
+  // },
+};
